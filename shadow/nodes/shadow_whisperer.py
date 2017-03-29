f1112a5d1b2ee0f6a3b050f8c6f6a668dc46c96d
WarrG3x <warrier.abhishek@gmail.com>
2017-03-29 18:19:55 +0530

    Waist Update

--- shadow/nodes/shadow_whisperer.py
+++ shadow/nodes/shadow_whisperer.py
@@ -18,9 +18,11 @@ class SkellAngler(object):
         self.HALF_PI = pi / 2.0
 
         rospy.Subscriber('skeleton', Skeleton, self.handler)
-        rospy.Publisher('skel_angles',String,queue_size=1)
-        rate = rospy.Rate(10)
-        self.msg= "n"
+        self.hand_pub = rospy.Publisher('skel_hands',String,queue_size=1)
+        self.leg_pub = rospy.Publisher('skel_legs',String,queue_size=1)
+        self.rate = rospy.Rate(10)
+        self.hand_msg= "n"
+        self.leg_msg = "n"
 
         self.skeleton = dict()
         self.skeleton['confidence'] = dict()
@@ -28,9 +30,12 @@ class SkellAngler(object):
         self.skeleton['orientation'] = dict()
 
         while not rospy.is_shutdown():
-            if msg != 'n':
-                pub.publish(msg)
-                rate.sleep()
+            if self.hand_msg != 'n':
+                self.hand_pub.publish(self.hand_msg)
+            if self.leg_msg != 'n':
+                self.leg_pub.publish(self.leg_msg)
+
+            self.rate.sleep()
 
 
 
@@ -58,7 +63,7 @@ class SkellAngler(object):
 
         right_arm_shoulder_lift_angle = math.degrees(-(asin(right_shoulder_elbow.y()) + self.HALF_PI))
         right_arm_shoulder_pan_angle = math.degrees(-asin(right_arm_elbow_flex_hand.x()))
-        right_arm_elbow_flex_angle = math.degrees(acos(KDL.dot(right_shoulder_elbow, right_arm_elbow_flex_hand)))
+        right_arm_elbow_flex_angle = 0.66*math.degrees(acos(KDL.dot(right_shoulder_elbow, right_arm_elbow_flex_hand)))
 
         # Left Arm
 
@@ -72,24 +77,51 @@ class SkellAngler(object):
 
         left_arm_shoulder_lift_angle = math.degrees(asin(left_shoulder_elbow.y()) + self.HALF_PI)
         left_arm_shoulder_pan_angle = math.degrees(asin(left_arm_elbow_flex_hand.x()))
-        left_arm_elbow_flex_angle = math.degrees(-acos(KDL.dot(left_shoulder_elbow, left_arm_elbow_flex_hand)))
+        left_arm_elbow_flex_angle = 0.66*math.degrees(-acos(KDL.dot(left_shoulder_elbow, left_arm_elbow_flex_hand)))
 
-        print "Right Lift ",
-        print right_arm_shoulder_lift_angle
-        print "Right Pan ",
-        print right_arm_shoulder_pan_angle
-        print "Right Elbow ",
-        print right_arm_elbow_flex_angle
+        right_femur = self.skeleton['position']['right_knee'] - self.skeleton['position']['right_hip']
+        right_femur.Normalize()
+        right_knee_angle = math.degrees((asin(right_femur.y()) + self.HALF_PI))
 
-        print "Left Lift ",
-        print left_arm_shoulder_lift_angle
-        print "Left Pan ",
-        print left_arm_shoulder_pan_angle
-        print "Left Elbow ",
-        print left_arm_elbow_flex_angle
 
-        self.msg = " ".join(map(str,[right_arm_shoulder_lift_angle,right_arm_shoulder_pan_angle,right_arm_elbow_flex_angle,
-                             left_arm_shoulder_lift_angle,left_arm_shoulder_pan_angle,left_arm_elbow_flex_angle]))
+        left_femur = self.skeleton['position']['left_knee'] - self.skeleton['position']['left_hip']
+        left_femur.Normalize()
+        left_knee_angle = math.degrees((asin(left_femur.y()) + self.HALF_PI))
+
+        waist = self.skeleton['position']['right_hip'] - self.skeleton['position']['left_hip']
+        waist.Normalize()
+        waist_angle = math.degrees((asin(waist.z())))
+        print waist_angle
+
+
+        # print "Right Knee",
+        # print right_knee_angle
+        # print "Left Knee",
+        # print left_knee_angle
+        # print "Right Lift ",
+        # print right_arm_shoulder_lift_angle
+        # print "Right Pan ",
+        # print right_arm_shoulder_pan_angle
+        # print "Right Elbow ",
+        # print right_arm_elbow_flex_angle
+        #
+        # print "Left Lift ",
+        # print left_arm_shoulder_lift_angle
+        # print "Left Pan ",
+        # print left_arm_shoulder_pan_angle
+        # print "Left Elbow ",
+        # print left_arm_elbow_flex_angle
+
+
+        self.hand_msg = " ".join(map(str, [right_arm_shoulder_lift_angle, left_arm_shoulder_lift_angle, right_arm_shoulder_pan_angle,
+                                      left_arm_shoulder_pan_angle,right_arm_elbow_flex_angle,left_arm_elbow_flex_angle]))
+
+        if left_knee_angle>25:
+            # print "Left"
+            self.leg_msg = "left"
+        elif right_knee_angle>25:
+            # print "Right"
+            self.leg_msg = "right"
 
 
 
